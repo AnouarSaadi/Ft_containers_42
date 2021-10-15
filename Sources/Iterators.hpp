639dc58987e0c2d06b295099c8cb30033c91bc57
a-saadi <anouarsaadi1802@gmail.com>
2021-10-15 11:46:18 +0100

    fix(ft_containers): Reimplemtation: iterator_traits, reverse_iterator and iterator.

--- Sources/Iterators.hpp
+++ Sources/Iterators.hpp
@@ -35,21 +35,21 @@ class IteratorTraits
 template <class T>
 class IteratorTraits<T*>
 {
-	typedef Iterator::difference_type difference_type;
-	typedef Iterator::value_type value_type;
-	typedef Iterator::pointer pointer;
-	typedef Iterator::reference reference;
-	typedef Iterator::iterator_category iterator_category;
+	typedef ptrdiff_t difference_type;
+	typedef T value_type;
+	typedef T* pointer;
+	typedef T& reference;
+	typedef std::random_access_iterator_tag iterator_category;
 };
 
 template <class T>
 class IteratorTraits<const T*>
 {
-	typedef Iterator::difference_type difference_type;
-	typedef Iterator::value_type value_type;
-	typedef Iterator::pointer pointer;
-	typedef Iterator::reference reference;
-	typedef Iterator::iterator_category iterator_category;
+	typedef ptrdiff_t difference_type;
+	typedef T value_type;
+	typedef const T* pointer;
+	typedef const T& reference;
+	typedef std::random_access_iterator_tag iterator_category;
 };
 
 /* reverse_iterator */
