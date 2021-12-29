684693bd22a1ec68ca062337beec12e2ef6c1b17
a-saadi <anouarsaadi1802@gmail.com>
2021-12-29 19:20:09 +0100

    fix: erase and insert function vector container

--- main_map.cpp
+++ main_map.cpp
@@ -25,32 +25,32 @@ int main() // ! erase need to check
 	{
 		std::cout << "\nbegin() & end() " << std::endl;
 		std::cout << "=======================================" << std::endl;
-		NS::set<int> map1;
-		map1.insert(5);
-		map1.insert(10);
-		map1.insert(4);
-		map1.insert(2);
-		NS::set<int>::iterator it = map1.begin();
-		print_map(map1, "map1");
-		std::cout << "map1.begin(): " << (*it).first << std::endl;
+		NS::set<int> set1;
+		set1.insert(5);
+		set1.insert(10);
+		set1.insert(4);
+		set1.insert(2);
+		NS::set<int>::iterator it = set1.begin();
+		print_map(set1, "set1");
+		std::cout << "set1.begin(): " << (*it) << std::endl;
 		std::cout << "=======================================" << std::endl;
 	}
 	// assign operator=()
 	{
 		std::cout << "\nassign operator=()" << std::endl;
 		std::cout << "=======================================" << std::endl;
-		NS::set<int> map1;
-		map1.insert(NS::make_pair(5, 1));
-		map1.insert(NS::make_pair(2, 2));
-		map1.insert(NS::make_pair(10, 3));
-		map1.insert(NS::make_pair(4, 4));
-		print_map(map1, "map1");
-		NS::set<int> map2;
-		std::cout << "before assigning map1 to map2" << std::endl;
-		print_map(map2, "map2");
-		map2 = map1;
-		std::cout << "after assigning map1 to map2" << std::endl;
-		print_map(map2, "map2");
+		NS::set<int> set1;
+		set1.insert(5);
+		set1.insert(2);
+		set1.insert(10);
+		set1.insert(4);
+		print_map(set1, "set1");
+		NS::set<int> set2;
+		std::cout << "before assigning set1 to set2" << std::endl;
+		print_map(set2, "set2");
+		set2 = set1;
+		std::cout << "after assigning set1 to set2" << std::endl;
+		print_map(set2, "set2");
 		std::cout << "=======================================" << std::endl;
 	}
 
@@ -59,15 +59,15 @@ int main() // ! erase need to check
 	{
 		std::cout << "\nclear()" << std::endl;
 		std::cout << "=======================================" << std::endl;
-		NS::set<int> map1;
-		map1.insert(NS::make_pair(5, 1));
-		map1.insert(NS::make_pair(2, 2));
-		map1.insert(NS::make_pair(10, 3));
-		map1.insert(NS::make_pair(4, 4));
-		print_map(map1, "map1");
-		map1.clear();
-		std::cout << "after clearing map1" << std::endl;
-		print_map(map1, "map1");
+		NS::set<int> set1;
+		set1.insert(5);
+		set1.insert(2);
+		set1.insert(10);
+		set1.insert(4);
+		print_map(set1, "set1");
+		set1.clear();
+		std::cout << "after clearing set1" << std::endl;
+		print_map(set1, "set1");
 		std::cout << "=======================================" << std::endl;
 	}
 
@@ -75,14 +75,14 @@ int main() // ! erase need to check
 	{
 		std::cout << "\ncount()" << std::endl;
 		std::cout << "=======================================" << std::endl;
-		NS::set<int> map1;
-		map1.insert(NS::make_pair(5, 1));
-		map1.insert(NS::make_pair(2, 2));
-		map1.insert(NS::make_pair(10, 3));
-		map1.insert(NS::make_pair(4, 4));
-		print_map(map1, "map1");
-		std::cout << "map1.count(5): " << map1.count(5) << std::endl;
-		std::cout << "map1.count(6): " << map1.count(6) << std::endl;
+		NS::set<int> set1;
+		set1.insert(5);
+		set1.insert(2);
+		set1.insert(10);
+		set1.insert(4);
+		print_map(set1, "set1");
+		std::cout << "set1.count(5): " << set1.count(5) << std::endl;
+		std::cout << "set1.count(6): " << set1.count(6) << std::endl;
 		std::cout << "=======================================" << std::endl;
 	}
 
@@ -90,17 +90,17 @@ int main() // ! erase need to check
 	{
 		std::cout << "\nempty()" << std::endl;
 		std::cout << "=======================================" << std::endl;
-		NS::set<int> map1;
-		map1.insert(NS::make_pair(5, 1));
-		map1.insert(NS::make_pair(2, 2));
-		map1.insert(NS::make_pair(10, 3));
-		map1.insert(NS::make_pair(4, 4));
-		print_map(map1, "map1");
-		std::cout << "map1.empty(): " << map1.empty() << std::endl;
-		map1.clear();
-		std::cout << "after clearing map1" << std::endl;
-		print_map(map1, "map1");
-		std::cout << "map1.empty(): " << map1.empty() << std::endl;
+		NS::set<int> set1;
+		set1.insert(5);
+		set1.insert(2);
+		set1.insert(10);
+		set1.insert(4);
+		print_map(set1, "set1");
+		std::cout << "set1.empty(): " << set1.empty() << std::endl;
+		set1.clear();
+		std::cout << "after clearing set1" << std::endl;
+		print_map(set1, "set1");
+		std::cout << "set1.empty(): " << set1.empty() << std::endl;
 		std::cout << "=======================================" << std::endl;
 	}
 
@@ -108,17 +108,17 @@ int main() // ! erase need to check
 	{
 		std::cout << "\nfind()" << std::endl;
 		std::cout << "=======================================" << std::endl;
-		NS::set<int> map1;
-		map1.insert(NS::make_pair(5, 1));
-		map1.insert(NS::make_pair(2, 2));
-		map1.insert(NS::make_pair(10, 3));
-		print_map(map1, "map1");
-		if (map1.find(5) != map1.end())
-			std::cout << "5 has been found and its second is = " << map1.find(5)->second << std::endl;
+		NS::set<int> set1;
+		set1.insert(5);
+		set1.insert(2);
+		set1.insert(10);
+		print_map(set1, "set1");
+		if (set1.find(5) != set1.end())
+			std::cout << "5 has been found and its second is = " << set1.find(5)->second << std::endl;
 		else
 			std::cout << "5 has not been found" << std::endl;
-		if (map1.find(6) != map1.end())
-			std::cout << "6 has been found and its second is = " << map1.find(6)->second << std::endl;
+		if (set1.find(6) != set1.end())
+			std::cout << "6 has been found and its second is = " << set1.find(6)->second << std::endl;
 		else
 			std::cout << "6 has not been found" << std::endl;
 		std::cout << "=======================================" << std::endl;
@@ -128,19 +128,14 @@ int main() // ! erase need to check
 	{
 		std::cout << "\ninsert()" << std::endl;
 		std::cout << "=======================================" << std::endl;
-		NS::set<int> map1;
-		map1.insert(NS::make_pair(5, 1));
-		map1.insert(NS::make_pair(2, 2));
-		map1.insert(NS::make_pair(10, 3));
-		print_map(map1, "map1");
-		NS::set<int>::iterator it = map1.insert(NS::make_pair(4, 4)).first;
-		print_map(map1, "map1");
-		std::cout << "inserted 4, 4 at " << (*it).first << std::endl;
-		NS::pair<int> arr[] = {NS::make_pair(0, 20), NS::make_pair(2, 30),
-									NS::make_pair(3, 40), NS::make_pair(4, 50)};
-		NS::set<int> map2;
-		map2.insert(arr, arr + 4);
-		print_map(map2, "map2");
+		NS::set<int> set1;
+		set1.insert(5);
+		set1.insert(2);
+		set1.insert(10);
+		print_map(set1, "set1");
+		NS::set<int>::iterator it = set1.insert(4).first;
+		print_map(set1, "set1");
+		std::cout << "inserted 4, 4 at " << (*it) << std::endl;
 		std::cout << "=======================================" << std::endl;
 	}
 
@@ -148,15 +143,13 @@ int main() // ! erase need to check
 	{
 		std::cout << "\nmap constructors" << std::endl;
 		std::cout << "=======================================" << std::endl;
-		NS::set<int> map1;
-		map1.insert(NS::make_pair(5, 1));
-		map1.insert(NS::make_pair(2, 2));
-		map1[10] = 3;
-		map1[4] = 4;
-		print_map(map1, "map1");
-		NS::set<int> map2(map1);
-		print_map(map2, "map2");
-		NS::set<int> map3(map2.begin(), map2.end());
+		NS::set<int> set1;
+		set1.insert(5);
+		set1.insert(2);
+		print_map(set1, "set1");
+		NS::set<int> set2(set1);
+		print_map(set2, "set2");
+		NS::set<int> map3(set2.begin(), set2.end());
 		print_map(map3, "map3");
 		std::cout << "=======================================" << std::endl;
 	}
@@ -165,23 +158,23 @@ int main() // ! erase need to check
 	{
 		std::cout << "\nerase()" << std::endl;
 		std::cout << "=======================================" << std::endl;
-		NS::set<int> map1;
-		map1.insert(NS::make_pair(0, 1));
-		map1.insert(NS::make_pair(2, 2));
-		map1.insert(NS::make_pair(10, 3));
-		map1.insert(NS::make_pair(4, 4));
-		map1.insert(NS::make_pair(3, 300));
-		map1.insert(NS::make_pair(3, 300));
-		map1.insert(NS::make_pair(1, 110));
-		map1.insert(NS::make_pair(0, 10));
-		print_map(map1, "map1");
-		std::cout << "map1.erase(0): " << map1.erase(0) << std::endl;
-		NS::set<int>::iterator it = map1.begin();
+		NS::set<int> set1;
+		set1.insert(0);
+		set1.insert(2);
+		set1.insert(10);
+		set1.insert(4);
+		set1.insert(3);
+		set1.insert(3);
+		set1.insert(1);
+		set1.insert(NS::make_pair(0, 10));
+		print_map(set1, "set1");
+		std::cout << "set1.erase(0): " << set1.erase(0) << std::endl;
+		NS::set<int>::iterator it = set1.begin();
 		std::advance(it, 2);
-		map1.erase(map1.begin(), it);
-		print_map(map1, "map1");
-		map1.erase(map1.begin());
-		print_map(map1, "map1");
+		set1.erase(set1.begin(), it);
+		print_map(set1, "set1");
+		set1.erase(set1.begin());
+		print_map(set1, "set1");
 
 		std::cout << "=======================================" << std::endl;
 	}
@@ -190,8 +183,8 @@ int main() // ! erase need to check
 	{
 		std::cout << "\nmax_size()" << std::endl;
 		std::cout << "=======================================" << std::endl;
-		NS::set<int> map1;
-		std::cout << "map1.max_size(): " << map1.max_size() << std::endl;
+		NS::set<int> set1;
+		std::cout << "set1.max_size(): " << set1.max_size() << std::endl;
 		std::cout << "=======================================" << std::endl;
 	}
 
@@ -199,21 +192,21 @@ int main() // ! erase need to check
 	{
 		std::cout << "\nswap()" << std::endl;
 		std::cout << "=======================================" << std::endl;
-		NS::set<int> map1;
-		map1.insert(NS::make_pair(5, 1));
-		map1.insert(NS::make_pair(2, 2));
-		map1.insert(NS::make_pair(10, 3));
-		map1.insert(NS::make_pair(4, 4));
-		print_map(map1, "map1");
-		NS::set<int> map2;
-		map2.insert(NS::make_pair(0, 1));
-		map2.insert(NS::make_pair(2, 2));
-		map2.insert(NS::make_pair(10, 3));
-		map2.insert(NS::make_pair(4, 4));
-		print_map(map2, "map2");
-		map1.swap(map2);
-		print_map(map1, "map1");
-		print_map(map2, "map2");
+		NS::set<int> set1;
+		set1.insert(5);
+		set1.insert(2);
+		set1.insert(10);
+		set1.insert(4);
+		print_map(set1, "set1");
+		NS::set<int> set2;
+		set2.insert(0);
+		set2.insert(NS::make_pair(2, 2));
+		set2.insert(10);
+		set2.insert(4);
+		print_map(set2, "set2");
+		set1.swap(set2);
+		print_map(set1, "set1");
+		print_map(set2, "set2");
 		std::cout << "=======================================" << std::endl;
 	}
 
@@ -221,18 +214,18 @@ int main() // ! erase need to check
 	{
 		std::cout << "\nlower_bound()" << std::endl;
 		std::cout << "=======================================" << std::endl;
-		NS::set<int> map1;
-		map1.insert(NS::make_pair(5, 1));
-		map1.insert(NS::make_pair(2, 2));
-		map1.insert(NS::make_pair(10, 3));
-		map1.insert(NS::make_pair(4, 4));
-		print_map(map1, "map1");
-		NS::set<int>::iterator it = map1.lower_bound(3);
-		std::cout << "map1.lower_bound(3): " << (*it).first << std::endl;
-		it = map1.lower_bound(5);
-		std::cout << "map1.lower_bound(5): " << (*it).first << std::endl;
-		it = map1.lower_bound(6);
-		std::cout << "map1.lower_bound(6): " << (*it).first << std::endl;
+		NS::set<int> set1;
+		set1.insert(5);
+		set1.insert(2);
+		set1.insert(10);
+		set1.insert(4);
+		print_map(set1, "set1");
+		NS::set<int>::iterator it = set1.lower_bound(3);
+		std::cout << "set1.lower_bound(3): " << (*it) << std::endl;
+		it = set1.lower_bound(5);
+		std::cout << "set1.lower_bound(5): " << (*it) << std::endl;
+		it = set1.lower_bound(6);
+		std::cout << "set1.lower_bound(6): " << (*it) << std::endl;
 		std::cout << "=======================================" << std::endl;
 	}
 
@@ -240,18 +233,18 @@ int main() // ! erase need to check
 	{
 		std::cout << "\nupper_bound()" << std::endl;
 		std::cout << "=======================================" << std::endl;
-		NS::set<int> map1;
-		map1.insert(NS::make_pair(5, 1));
-		map1.insert(NS::make_pair(2, 2));
-		map1.insert(NS::make_pair(10, 3));
-		map1.insert(NS::make_pair(4, 4));
-		print_map(map1, "map1");
-		NS::set<int>::iterator it = map1.upper_bound(3);
-		std::cout << "map1.upper_bound(3): " << (*it).first << std::endl;
-		it = map1.upper_bound(5);
-		std::cout << "map1.upper_bound(5): " << (*it).first << std::endl;
-		it = map1.upper_bound(6);
-		std::cout << "map1.upper_bound(6): " << (*it).first << std::endl;
+		NS::set<int> set1;
+		set1.insert(5);
+		set1.insert(2);
+		set1.insert(10);
+		set1.insert(4);
+		print_map(set1, "set1");
+		NS::set<int>::iterator it = set1.upper_bound(3);
+		std::cout << "set1.upper_bound(3): " << (*it) << std::endl;
+		it = set1.upper_bound(5);
+		std::cout << "set1.upper_bound(5): " << (*it) << std::endl;
+		it = set1.upper_bound(6);
+		std::cout << "set1.upper_bound(6): " << (*it) << std::endl;
 		std::cout << "=======================================" << std::endl;
 	}
 
@@ -259,18 +252,18 @@ int main() // ! erase need to check
 	{
 		std::cout << "\nequal_range()" << std::endl;
 		std::cout << "=======================================" << std::endl;
-		NS::set<int> map1;
-		map1.insert(NS::make_pair(5, 1));
-		map1.insert(NS::make_pair(2, 2));
-		map1.insert(NS::make_pair(10, 3));
-		map1.insert(NS::make_pair(4, 4));
-		print_map(map1, "map1");
-		NS::pair<NS::set<int>::iterator, NS::set<int>::iterator> range = map1.equal_range(3);
-		std::cout << "map1.equal_range(3): " << (*range.first).first << " " << (*range.second).first << std::endl;
-		range = map1.equal_range(5);
-		std::cout << "map1.equal_range(5): " << (*range.first).first << " " << (*range.second).first << std::endl;
-		range = map1.equal_range(6);
-		std::cout << "map1.equal_range(6): " << (*range.first).first << " " << (*range.second).first << std::endl;
+		NS::set<int> set1;
+		set1.insert(5);
+		set1.insert(2);
+		set1.insert(10);
+		set1.insert(4);
+		print_map(set1, "set1");
+		NS::pair<NS::set<int>::iterator, NS::set<int>::iterator> range = set1.equal_range(3);
+		std::cout << "set1.equal_range(3): " << (*range.first).first << " " << (*range.second).first << std::endl;
+		range = set1.equal_range(5);
+		std::cout << "set1.equal_range(5): " << (*range.first).first << " " << (*range.second).first << std::endl;
+		range = set1.equal_range(6);
+		std::cout << "set1.equal_range(6): " << (*range.first).first << " " << (*range.second).first << std::endl;
 		std::cout << "=======================================" << std::endl;
 	}
 
@@ -278,9 +271,9 @@ int main() // ! erase need to check
 	{
 		std::cout << "\nget_allocator()" << std::endl;
 		std::cout << "=======================================" << std::endl;
-		NS::set<int> map1;
-		print_map(map1, "map1");
-		NS::set<int>::allocator_type alloc = map1.get_allocator();
+		NS::set<int> set1;
+		print_map(set1, "set1");
+		NS::set<int>::allocator_type alloc = set1.get_allocator();
 		NS::pair<const int, int> *ptr = alloc.allocate(1);
 		alloc.deallocate(ptr, 1);
 		std::cout << "alloc max size: " << alloc.max_size() << std::endl;
@@ -292,17 +285,17 @@ int main() // ! erase need to check
 	{
 		std::cout << "\nconst iterators" << std::endl;
 		std::cout << "=======================================" << std::endl;
-		NS::set<int> map1;
-		map1.insert(NS::make_pair(5, 1));
-		map1.insert(NS::make_pair(2, 2));
-		map1.insert(NS::make_pair(10, 3));
-		map1.insert(NS::make_pair(4, 4));
-		print_map(map1, "map1");
-		NS::set<int>::const_iterator it = map1.begin();
-		std::cout << "map1.begin(): " << (*it).first << std::endl;
-		it = map1.end();
+		NS::set<int> set1;
+		set1.insert(5);
+		set1.insert(2);
+		set1.insert(10);
+		set1.insert(4);
+		print_map(set1, "set1");
+		NS::set<int>::const_iterator it = set1.begin();
+		std::cout << "set1.begin(): " << (*it) << std::endl;
+		it = set1.end();
 		--it;
-		std::cout << "map1.end(): " << (*it).first << std::endl;
+		std::cout << "set1.end(): " << (*it) << std::endl;
 		std::cout << "=======================================" << std::endl;
 	}
 
@@ -310,17 +303,17 @@ int main() // ! erase need to check
 	{
 		std::cout << "\nreverse iterators" << std::endl;
 		std::cout << "=======================================" << std::endl;
-		NS::set<int> map1;
-		map1.insert(NS::make_pair(5, 1));
-		map1.insert(NS::make_pair(2, 2));
-		map1.insert(NS::make_pair(10, 3));
-		map1.insert(NS::make_pair(4, 4));
-		print_map(map1, "map1");
-		NS::set<int>::reverse_iterator it = map1.rbegin();
+		NS::set<int> set1;
+		set1.insert(5);
+		set1.insert(2);
+		set1.insert(10);
+		set1.insert(4);
+		print_map(set1, "set1");
+		NS::set<int>::reverse_iterator it = set1.rbegin();
 		// print all elements in reverse order
-		for (; it != map1.rend(); ++it)
+		for (; it != set1.rend(); ++it)
 		{
-			std::cout << "[" << (*it).first << "] = " << (*it).second << std::endl;
+			std::cout << "[" << (*it) << "] = " << (*it) << std::endl;
 		}
 
 		std::cout << "=======================================" << std::endl;
